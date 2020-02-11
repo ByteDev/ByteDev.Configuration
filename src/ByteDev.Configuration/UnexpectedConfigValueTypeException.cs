b6088447adb520693c0c2992c1ceb8a03210595d
bytedev <paulstanley@protonmail.com>
2020-02-11 22:02:14 +0800

    Added xmldoc for custom exception

--- src/ByteDev.Configuration/UnexpectedConfigValueTypeException.cs
+++ src/ByteDev.Configuration/UnexpectedConfigValueTypeException.cs
@@ -3,29 +3,54 @@ using System.Runtime.Serialization;
 
 namespace ByteDev.Configuration
 {
+    /// <summary>
+    /// Represents an unexpected value type in config.
+    /// </summary>
     [Serializable]
     public class UnexpectedConfigValueTypeException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:ByteDev.Configuration.UnexpectedConfigValueTypeException" /> class.
+        /// </summary>
         public UnexpectedConfigValueTypeException()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:ByteDev.Configuration.UnexpectedConfigValueTypeException" /> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
         public UnexpectedConfigValueTypeException(string message) : base(message)
         {
         }
 
-        public UnexpectedConfigValueTypeException(string message, Exception inner) : base(message, inner)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:ByteDev.Configuration.UnexpectedConfigValueTypeException" /> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>       
+        public UnexpectedConfigValueTypeException(string message, Exception innerException) : base(message, innerException)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:ByteDev.Configuration.UnexpectedConfigValueTypeException" /> class.
+        /// </summary>
+        /// <param name="key">The config key.</param>
+        /// <param name="value">The config value.</param>
+        /// <param name="expectedType">The expected type in config.</param>
         public UnexpectedConfigValueTypeException(string key, string value, Type expectedType)
-            : base(string.Format("Key: '{0}' value: '{1}' is not of expected type: {2}.", key, value, expectedType.Name))
+            : base($"Key: '{key}' value: '{value}' is not of expected type: {expectedType.Name}.")
         {
         }
 
-        protected UnexpectedConfigValueTypeException(
-            SerializationInfo info,
-            StreamingContext context) : base(info, context)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:ByteDev.Configuration.UnexpectedConfigValueTypeException" /> class.
+        /// </summary>
+        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"></see> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext"></see> that contains contextual information about the source or destination.</param>
+        protected UnexpectedConfigValueTypeException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
         {
         }
     }
